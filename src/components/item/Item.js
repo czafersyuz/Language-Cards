720e3952fb13b27ae1f6beeec4fe81293e0c7471
czafersyuz <czafersyuz@gmail.com>
2022-03-26 17:33:26 +0300

    first commit

---
+++ src/components/item/Item.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../card/Card';
+
+const Item = ({ card }) => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h3> {Card.name} </h3>
+      {
+
+      }
+    </div>
+  )
+}
+
+export default Item;
